06e9647d428455d78b6933de3ee30a65570c512e
unknown <sam.raj@incredible-web.com>
2020-10-03 15:19:34 +0200

    comment

--- src/service/constants.js
+++ src/service/constants.js
@@ -2,4 +2,4 @@ export const API_URL = `https://picsum.photos/v2`;
 export const DEFAULT_PAGE_LIMIT = 30;
 export const DEFAULT_PAGE_NUMBER = 1;
 export const MAXIMUM_PAGES = 5;
-export const TOTAL_ITEMS = 993;
+export const TOTAL_ITEMS = 993; // Manually counted total number
